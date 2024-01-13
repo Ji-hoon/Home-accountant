c7726f94fe1ba2992ae78712e1ecc93f51802df8
[SW7] 김지훈 <lanslot84@gmail.com>
2024-01-13 17:04:48 +0900

    Chore : [BE] db 연결 및 서버 환경 변수 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,38 +1,37 @@
 import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-//import "dotenv/config";
+import mongoose from "mongoose";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 
-// const { PORT, MONGODB_URL, FRONTEND_URL } = process.env;
-// if (!PORT || !MONGODB_URL || !FRONTEND_URL) {
-//   console.error("no env var");
-//   process.exit();
-// }
+const { PORT, MONGODB_URL, FRONTEND_URL } = process.env;
+if (!PORT || !MONGODB_URL || !FRONTEND_URL) {
+  console.error("no env var");
+  process.exit();
+}
 
-// mongoose.connect(MONGODB_URL);
-// mongoose.connection.on("connected", () => {
-//   console.log("Successfully connected to MongoDB");
-// });
+mongoose.connect(MONGODB_URL);
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to MongoDB");
+});
 
 const app = express();
+app.use(cookieParser());
 
 // app.use(errorLogger);
-
-// app.use(express.json());
-// app.use(
-//   cors({
-//     origin: [FRONTEND_URL],
-//     credentials: true,
-//   }),
-// );
-//app.use(cookieParser());
-
 //app.use(errorHandler);
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: [FRONTEND_URL],
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello, Home accountant server!");
 });
-app.listen("5000", () => {
-  console.log(`PORT:5000`);
+app.listen(PORT, () => {
+  console.log(`PORT:${PORT}`);
 });
