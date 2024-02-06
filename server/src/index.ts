df4d53c66040ba97b81e1043489142c09bda38bf
[SW7] 김지훈 <lanslot84@gmail.com>
2024-02-06 16:43:20 +0900

    Refactor: [BE] ip 주소 변경

--- server/src/index.ts
+++ server/src/index.ts
@@ -65,6 +65,6 @@ app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "./dist/index.html"));
 });
 
-app.listen(PORT as unknown as number, "127.0.0.1", () => {
-  console.log(`PORT:${PORT} IP: ${"127.0.0.1"}`);
+app.listen(PORT as unknown as number, "0.0.0.0", () => {
+  console.log(`PORT:${PORT} IP: "0.0.0.0"`);
 });
