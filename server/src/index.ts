3122b8df2620be5fb9d48873a574c6b2532921b5
[SW7] 김지훈 <lanslot84@gmail.com>
2024-02-07 09:41:34 +0900

    Refactor: [BE] static path 수정

--- server/src/index.ts
+++ server/src/index.ts
@@ -51,8 +51,8 @@ app.use("/api/asset_types", assetTypeRouter);
 
 app.use(errorHandler);
 
-app.use(express.static(join(__dirname, "./dist")));
 app.use("/", express.static(join(__dirname, "dist")));
+
 app.use(
   express.static(join(__dirname, "/"), {
     setHeaders: (res, path) => {
