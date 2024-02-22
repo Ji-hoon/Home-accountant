926ba03fa2f2a7fe69164726e29f40744d71a10a
[SW7] 김지훈 <lanslot84@gmail.com>
2024-02-22 11:53:40 +0900

    Feat: [FE] mobile dropdown test animation 적용

--- client/src/components/dropdown/Dropdown.tsx
+++ client/src/components/dropdown/Dropdown.tsx
@@ -94,6 +94,21 @@ export const DropdownUIContainerStyle = styled.div<{
       border-radius: 5px 5px 0 0;
       overflow-y: auto;
       width: 100%;
+
+      -webkit-animation: bottom-up 150ms cubic-bezier(0.44, 0.13, 0.37, 1.07)
+        forwards;
+      animation: bottom-up 150ms cubic-bezier(0.44, 0.13, 0.37, 1.07) forwards;
+    }
+
+    @keyframes bottom-up {
+      0% {
+        transform: translateY(20vh);
+        opacity: 0;
+      }
+      100% {
+        transform: translateY(0px);
+        opacity: 1;
+      }
     }
   }
 `;
