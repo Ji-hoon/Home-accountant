16890bbdf9517f218e3aff7f92b9da741e71f90b
[SW7] 김지훈 <lanslot84@gmail.com>
2024-03-11 09:49:45 +0900

    Chore: [FE] update vitest setting

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,8 +14,11 @@ export default defineConfig({
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
+    //alias: [{ find: /^@visx/scale$/, replacement: '@visx/scale/esm/index.js' }],
   },
   resolve: {
+    // added mainFields for ERR_REQUIRE_ESM error
+    mainFields: ["module"],
     alias: {
       "@": "/src",
     },
